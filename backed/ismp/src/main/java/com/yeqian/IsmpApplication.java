2ac8b067c2d377da06ca7e89613426cf6720b6a2
huanggengbiao <2864201368@qq.com>
2025-07-21 09:59:15 +0800

    code:项目构建

---
+++ backed/ismp/src/main/java/com/yeqian/IsmpApplication.java
@@ -0,0 +1,13 @@
+package com.yeqian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsmpApplication.class, args);
+    }
+
+}
---
+++ backed/ismp/src/test/java/com/yeqian/IsmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeqian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsmpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
