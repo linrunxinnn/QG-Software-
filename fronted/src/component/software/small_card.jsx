904ee34f4be97a6255d486392bcd5d079d4be9a1
linrunxinnn <lrxczy03140125@gmail.com>
2025-07-23 12:09:31 +0800

    暂存

--- fronted/src/component/software/small_card.jsx
+++ fronted/src/component/software/small_card.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import './small_card.css'; // 样式文件同步更新
+import React from "react";
+import "./small_card.css"; // 样式文件同步更新
 
 const SmallCard = () => {
-    return (
-        <div className="card">
-            <div className="card-img"></div>
-            <div className="card-info">
-                <div className="card-title">名称</div>
-                <div className="card-price">¥19.90</div>
-            </div>
-            <button className="download-btn">下载</button>
-        </div>
-    );
+  return (
+    <div className="card">
+      <div className="card-img"></div>
+      <div className="card-info">
+        <div className="card-title">名称</div>
+        <div className="card-price">¥19.90</div>
+      </div>
+      <button className="download-btn">下载</button>
+    </div>
+  );
 };
-
+//wozaifenzhi shang xiugai
 export default SmallCard;
